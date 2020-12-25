a61feb612dfcfabc0a8dc931f3fc637faa86d306
Mengdi-hub <55174925+Mengdi-hub@users.noreply.github.com>
2020-12-25 15:30:45 -0800

    Update urls.py

--- learning_templates/learning_templates/urls.py
+++ learning_templates/learning_templates/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import url, include
 from basic_app import views
 
 urlpatterns = [
